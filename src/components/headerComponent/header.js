7a5d007d04ab8faa0cb894c569fcdc48aa1f9ce0
hysumi <henrique.sumitomo@gmail.com>
2018-06-10 16:18:03 -0300

    Adicionar component Header

---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+// components
+
+class Header extends Component {
+    render () {
+        return (
+            <header>
+                Test Header
+            </header>
+        );
+    }
+}
+
+export default Header;
