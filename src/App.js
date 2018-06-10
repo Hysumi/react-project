dcb4fe759f249b9a8650c91be4ff5ac4e92733d0
henrique.sumitomo@gmail.com <henrique.sumitomo@gmail.com>
2018-06-10 02:34:58 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-console.log("te s");
+console.log("teste");
 class App extends Component {
     render() {
         return <div className="App">Test</div>;
