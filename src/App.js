b4385d314647d15c68b2bceabf3fc26c6198f083
hysumi <henrique.sumitomo@gmail.com>
2018-06-10 03:26:36 -0300

    teste lint hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 
-console.log("teste")        ;
-
-
-
-
-
+console.log("teste") ;
 
 
 class App extends Component {
-    render() {
+    render () {
         return <div className="App">Test</div>;
     }
 }
