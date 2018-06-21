3c1f05fe1e438f18364964837065416b3ddbdc8d
Vinicius Georgetti Palharin <vinicius.palharin@linx.com.br>
2018-06-21 18:42:39 -0300

    Correções menores na home

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -6,18 +6,12 @@ import Form from "./../formComponent/form";
 class HomePage extends Component {
 
     state = {
-        fields: {}
-    }
-
-    onSubmit = (fields) => {
-        var x = fields;
-        console.log(x);
     }
 
     render () {
         return (
             <div>
-                <Form onSubmit={(fields) => this.onSubmit(fields)}/>
+                <Form />
             </div>
         );
     }
