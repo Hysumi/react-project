fdc8cadfcfe0a85bbfa7fa44c730ff2c45047e7c
Henrique Sumitomo <henrique.sumitomo@linx.com.br>
2018-06-21 12:59:55 -0300

    Adicionando identificador unico à lista de objetos

--- src/components/formComponent/form.js
+++ src/components/formComponent/form.js
@@ -7,67 +7,65 @@ export default class Form extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            id: [],
+            attributes: [],
+            id: 0
         };
         this.addAttribute = this.addAttribute.bind(this);
         this.deleteAttribute = this.deleteAttribute.bind(this);
     }
 
-    addAttribute () {
-        let hasNull = false;
-        let _state = this.state;
-        let cont = 0;
-        while (cont < _state.id.length && !hasNull) {
-            if (!_state.id[cont] && _state.id[cont] !== 0) {
-                _state.id[cont] = cont;
-                hasNull = true;
-                this.setState({id: _state});
-            }
-            cont++;
-        }
-        if (!hasNull) {
-            this.state.id.push(this.state.id.length);
-        }
-        this.setState(this.state);
-        console.log(this.state.id);
-    }
-
-    deleteAttribute (v) {
-        for (var i = 0; i < this.state.id.length; i++) {
-            if (this.state.id[i] === v) {
-                delete this.state.id[i];
-            }
-        }
+    addAttribute (e) {
+        e.preventDefault();
+        const newAttribute = {id: this.state.id, key: "", value: ""};
+        let id = this.state.id + 1;
+        let attributes = [...this.state.attributes, newAttribute];
         this.setState({
-            id:this.state.id
+            id,
+            attributes
         });
-        console.log(this.state.id);
     }
 
-    getInputOfType (type) {
+    deleteAttribute (id) {
+        let _state = [];
+        for (var i = 0; i < this.state.attributes.length; i++) {
+            if (this.state.attributes[i].id === id) {
+                delete this.state.attributes[i];
+                continue;
+            }
+            _state.push(this.state.attributes[i]);
+        }
+        this.setState({attributes: _state});
+    }
+
+    getInputOfType (attribute, type = "default") {
         let component;
         switch (type) {
         default:
             component = (
-                <InputKeyValue />
+                <InputKeyValue
+                    // attribute
+                />
             );
             break;
         }
         return component;
     }
 
+    setUniqueId () {
+
+    }
+
     render () {
-        let { id } = this.state;
         return (
             <div>
-                <button className="addbutton" onClick={this.addAttribute}>Add Attribute</button>
-                {id.map((attribute, i) => {
+                <button onClick={(e) => this.addAttribute(e)}>Add Attribute</button>
+                {this.state.attributes.map((attr) => {
                     return (
-                        <div key={i}>
-                            {this.getInputOfType(attribute)}
-                            <button onClick={this.deleteAttribute.bind(this, attribute)}>
+                        <div key={attr.id}>
+                            {this.getInputOfType(attr)}
+                            <button onClick={this.deleteAttribute.bind(this, attr.id)}>
                                 Delete Attribute
-                            </button>{attribute}
+                            </button>
                         </div>
                     );
                 })}
